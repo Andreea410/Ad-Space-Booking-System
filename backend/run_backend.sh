1efd28a57f4ae8ca5e795320032f6d9ca2ff6636
Ungur Andreea <147054941+Andreea410@users.noreply.github.com>
2025-11-30 16:32:33 +0200

    Change DB_USER and DB_PASS in run_backend.sh
    Updated database credentials in run_backend.sh

--- backend/run_backend.sh
+++ backend/run_backend.sh
@@ -3,9 +3,8 @@
 # Set environment variables
 export DB_HOST=localhost
 export DB_PORT=5432
-export DB_USER=mario
-export DB_PASS=supersecret
-export SOME_API_KEY=abc123
+export DB_USER=postgres # add your username here 
+export DB_PASS=postgres # add your password here
 
 # Add anything else your project needs
 
